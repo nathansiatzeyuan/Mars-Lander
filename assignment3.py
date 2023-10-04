4b999b6b147c4b6bf39c1175be3aa7a6d6921e3a
Nathan Sia <111715302+nathansiatzeyuan@users.noreply.github.com>
2023-10-04 08:19:21 +0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,1 +1,13 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+results = np.loadtxt('trajectories.txt')
+print(results)
+plt.figure(1)
+plt.clf()
+plt.xlabel('time (s)')
+plt.grid()
+plt.plot(results[:, 0], results[:, 1], label='x (m)')
+plt.plot(results[:, 0], results[:, 2], label='v (m/s)')
+plt.legend()
+plt.show()
